58a736c037b9eff0cc226efef100403304e944db
u17754640 <galuzin.l.a@sberbank.ru>
2019-10-31 16:02:56 +0300

    spring override bean

--- spring-ioc/src/main/java/ru/galuzin/spring/ioc/config/ServicesConfig.java
+++ spring-ioc/src/main/java/ru/galuzin/spring/ioc/config/ServicesConfig.java
@@ -1,9 +1,12 @@
 package ru.galuzin.spring.ioc.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import ru.galuzin.spring.ioc.dao.PersonDao;
+import ru.galuzin.spring.ioc.dao.PersonDaoRandom;
+import ru.galuzin.spring.ioc.dao.PersonDaoSimple;
 import ru.galuzin.spring.ioc.service.OtherService;
 import ru.galuzin.spring.ioc.service.PersonService;
 import ru.galuzin.spring.ioc.service.PersonServiceImpl;
@@ -26,5 +29,15 @@ public class ServicesConfig {
     @Bean
     public StartStopService startStopService(){
         return new StartStopService();
+    }
+
+    /**
+     * override bean
+     * @return
+     */
+    @Bean
+    public PersonDao personDao() {
+        System.out.println("create random dao");
+        return new PersonDaoRandom("not injected","not injected");
     }
 }
