54482d85e9f28bd36a63d7e083f7ed17caa11647
u17754640 <galuzin.l.a@sberbank.ru>
2019-11-18 18:16:39 +0300

    spring orm

--- spring-orm/src/main/java/ru/pesok/graviy/spring/orm/Main.java
+++ spring-orm/src/main/java/ru/pesok/graviy/spring/orm/Main.java
@@ -4,8 +4,9 @@ package ru.pesok.graviy.spring.orm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.pesok.graviy.spring.orm.domain.Person;
+import ru.pesok.graviy.spring.orm.domain.TaskType;
 import ru.pesok.graviy.spring.orm.service.PersonService;
+import ru.pesok.graviy.spring.orm.service.TaskService;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -21,19 +22,25 @@ public class Main {
     @Autowired
     private PersonService service;
 
+    @Autowired
+    private TaskService taskService;
+
     @PostConstruct
     public void init() {
         System.out.println("init start");
-        service.save(new Person(1,"Pushkin","room"));
-        service.save(new Person(2,"Errt","kitchen"));
-        service.save(new Person(3,"Ghh","room"));
-        service.save(new Person(4,"Zxx","room"));
-        service.save(new Person(5,"Xcc","kitchen"));
-        //service.save(new Person(1,"Pushkin"));
-        List<Person> all = service.getAll();
-        System.out.println("before "+all);
-        service.deleteFromZone("kitchen");
-        all = service.getAll();
-        System.out.println("after "+all);
+//        taskService.save(new TaskType(7,"DATA_TEST"));
+        List<TaskType> all = taskService.getAll();
+        System.out.println("all = " + all);
+//        service.save(new Person(1,"Pushkin","room"));
+//        service.save(new Person(2,"Errt","kitchen"));
+//        service.save(new Person(3,"Ghh","room"));
+//        service.save(new Person(4,"Zxx","room"));
+//        service.save(new Person(5,"Xcc","kitchen"));
+//        //service.save(new Person(1,"Pushkin"));
+//        List<Person> all = service.getAll();
+//        System.out.println("before "+all);
+//        service.deleteFromZone("kitchen");
+//        all = service.getAll();
+//        System.out.println("after "+all);
     }
 }
---
+++ spring-orm/src/main/java/ru/pesok/graviy/spring/orm/domain/TaskType.kt
@@ -0,0 +1,13 @@
+package ru.pesok.graviy.spring.orm.domain
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "TASK_TYPE")
+data class TaskType(
+        @Id @Column(name = "ID", nullable = false) val id: Long = -1,
+        @Column(name = "NAME", nullable = false) val name: String = "undefined"
+)
