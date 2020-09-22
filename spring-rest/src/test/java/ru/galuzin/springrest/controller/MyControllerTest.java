997b10ee1459ee9a6a543e20057cf2f913c5d1ed
Leonid <galuzin.leonid@gmail.com>
2020-09-22 11:42:21 +0300

    spring rest test

---
+++ spring-rest/src/test/java/ru/galuzin/springrest/controller/MyControllerTest.java
@@ -0,0 +1,57 @@
+package ru.galuzin.springrest.controller;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MyControllerTest.TestConfig.class, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@TestPropertySource(
+        locations = "classpath:application-test.properties")
+public class MyControllerTest implements ApplicationContextAware {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    public void test() throws Exception {
+        mvc.perform(post("/api/v1/testpost")).andExpect(status().isAccepted());
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        String[] allBeanNames = applicationContext.getBeanDefinitionNames();
+        for(String beanName : allBeanNames) {
+            System.out.println(beanName);
+        }
+    }
+
+    @Configuration
+    @EnableAutoConfiguration
+    @ComponentScan(basePackages = {
+            "ru.galuzin.springrest.controller"
+    }
+//    ,
+//            excludeFilters = {
+//                    @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = WebApplication.class)
+//            }
+    )
+    static class TestConfig {
+    }
+}
