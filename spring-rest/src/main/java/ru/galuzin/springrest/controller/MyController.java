190c50597b9605a33eaa5e4dc0cb1c961f4aa6cc
Leonid <galuzin.leonid@gmail.com>
2021-02-19 16:13:37 +0300

    value in constructor

--- spring-rest/src/main/java/ru/galuzin/springrest/controller/MyController.java
+++ spring-rest/src/main/java/ru/galuzin/springrest/controller/MyController.java
@@ -1,5 +1,6 @@
 package ru.galuzin.springrest.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/api/v1")
 public class MyController {
+
+    private final String setting1;
+
+    public MyController(@Value("${setting1}")String setting1) {
+        this.setting1 = setting1;
+    }
 
     @PostMapping("/testpost")
     public ResponseEntity<String> testpost(){
